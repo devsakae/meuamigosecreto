7cae783a8b71c342a51c664e5b82f5d4b4414edd
Rodrigo Sakae <devsakae@gmail.com>
2023-05-02 10:46:30 -0300

    fix: somevariables

--- pages/api/gifts.js
+++ pages/api/gifts.js
@@ -1,16 +1,16 @@
-// import clientPromise from "../../lib/mongodb";
+import clientPromise from "../../lib/mongodb";
 
-// export default async (req, res) => {
-//    try {
-//        const client = await clientPromise;
-//        const db = client.db("masdatabase");
-//        const gifts = await db
-//            .collection("gifts")
-//            .find({})
-//            .limit(10)
-//            .toArray();
-//        res.json(gifts);
-//    } catch (e) {
-//        console.error(e);
-//    }
-// };
+export default async (req, res) => {
+   try {
+       const client = await clientPromise;
+       const db = client.db("masdatabase");
+       const gifts = await db
+           .collection("gifts")
+           .find({})
+           .limit(10)
+           .toArray();
+       res.json(gifts);
+   } catch (e) {
+       console.error(e);
+   }
+};
